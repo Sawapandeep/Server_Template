71a58a98974f78d9c9ef9d116a5888505538e4ab
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2024-09-12 11:32:08 +0530

    setting up GET endpoint

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const axios = require('axios');
+const address="https://jsonplaceholder.typicode.com/";
 const app=express();
 const PORT=3000 || process.env.PORT;
 
-app.get('/named-port',(req,res)=>{});
+
+//! A simple get endpoint
+
+app.get('/users',async(req,res)=>{
+    try{
+   const response=await  axios.get(address+'users');
+        console.log('data fetched 👍🏻')
+        res.status(200).json(response.data); 
+    }
+     catch(error){
+        res.status(500).json({ message: 'Error fetching data 🥹' });
+    }
+});
+
+
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}/`);
 })
